e8d7957a537455d21a154b179a0e0a4574c06491
FarzeenZainab <farzeenzaidi51@gmail.com>
2023-03-21 15:55:53 +0500

    section 15

---
+++ Section 15 - Project (Working with custom hooks)/src/App.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/use-http";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformData = (tasksData) => {
+    const loadedTasks = [];
+
+    for (const taskKey in data) {
+      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+
+  // Setting HTTP parameters for the hook
+  const requestConfig = {
+    url: "https://taks-udemy-default-rtdb.firebaseio.com/tasks.json",
+  };
+  const {
+    isLoading,
+    error,
+    sendRequest: fe,
+  } = useHttp(requestConfig, transformData);
+
+  // https://taks-udemy-default-rtdb.firebaseio.com/tasks.json
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://taks-udemy-default-rtdb.firebaseio.com/tasks.json"
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Request failed!");
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  // useEffect(() => {
+  //   fetchTasks();
+  // }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
